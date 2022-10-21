10dcd079f92b95b261c0a1cdcb09f9eadc9d1e65
bora.gokbakan <bora.gokbakan@kobil.com>
2022-10-21 09:34:59 +0200

    First commit

---
+++ explore.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+pirate_attacks = pd.read_csv('data/pirate_attacks.csv')
+country_indicators = pd.read_csv('data/country_indicators.csv')
+country_codes = pd.read_csv('data/country_codes.csv')
+
+from collections import Counter
+from pprint import pprint
+c_vessel_types = Counter(pirate_attacks.vessel_type)
+pprint(c_vessel_types)
+pass
