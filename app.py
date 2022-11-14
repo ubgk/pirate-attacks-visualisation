3210d2ed8714991ac65b87b61f8b0297c6c03f28
bora.gokbakan <bora.gokbakan@kobil.com>
2022-11-14 17:49:10 +0100

    first viz

---
+++ app.py
@@ -0,0 +1,15 @@
+import dash
+
+import visualizations.root
+
+external_stylesheets = [
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+]
+
+app = dash.Dash(__name__, title='Global Maritime Attacks', external_stylesheets=external_stylesheets,
+                suppress_callback_exceptions=True)
+app.layout = visualizations.root.layout
+server = app.server
+
+if __name__ == '__main__':
+    app.run_server(debug=True)  # This seems to run settings.py once more.
